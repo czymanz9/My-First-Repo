a0dc4ec745389c388e45f7bb27bd23178c395ddb
czymanz9 <czyrahmanzano@gmail.com>
2025-03-02 21:49:27 +0800

    loops

---
+++ JavaScript/javaScript-loops-assignment/LoopsPractice.js
@@ -0,0 +1,26 @@
+// Task 1
+let number = 9;
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${number} x ${i} = ${number * i}`);
+};
+
+// Task 2
+let num = 6;
+let sum = 0;
+
+for(let i = 1; i <= num; i++){
+    sum += i;
+};
+
+console.log(`The sum of the first ${num} numbers is: ${sum}`);
+
+//Task 3
+let nums = [1,9,3,8,5,8];
+
+for(let i = 0; i < nums.length; i++){
+    console.log(`Array Element: ${nums[i]}`);
+};
+
+//Task 4
+
