05d8030367cccae4a32b8f4715a9422429cbbe99
czymanz9 <czyrahmanzano@gmail.com>
2025-03-07 12:28:59 +0800

    Random Charac generator done

--- JavaScript/Random Character Generator/script.js
+++ JavaScript/Random Character Generator/script.js
@@ -1,60 +1,48 @@
-/* 1. Define Two Warrior Objects */
-let warrior1 = {
-    name: "Thor" ,
-    hp: 100,
-    strength: 20,
-    attack() {
-        return Math.floor(Math.random() * (this.strength + 1));
+/*
+1. Create a Character Object
+2. Add a Random Class
+3. Add Randomized Health
+4. Assign a Special Ability
+5. Create a generateCharacter() Function 
+6. Battle Simulation
+*/
+/* Generates a character randomly with its details, the name can be defined if you want it to be
+specific, the others are just produced randomly */
+/* The battle method logs the 2 character's name and the so called battle when one character attacks the other with an attack of 5 to 20 which decreases the opponents health*/
+function  generateCharacter(name){
+    let character = {
+        name: name || ["Thorin", "Elara", "Zane", "Ivy"][Math.floor(Math.random() * 4)] ,
+        health: 100,
+        randomizeHealth(){
+            this.health = Math.floor(Math.random() * 101) + 50;
+        },
+        class: ["Warrior", "Mage", "Archer", "Healer", "Assassin"][Math.floor(Math.random() * 5)],
+        specialAbility: ["Fireball", "Healing Touch", "Stealth", "Lightning Strike", "Power Slash"][Math.floor(Math.random() * 5)],
+        battle(character2){
+            let attack = Math.floor(Math.random() * 21) + 5;
+            return console.log(`Character ${this.name} attacked Character ${character2.name} with ${this.specialAbility}. ${character2.name}'s health dropped from ${character2.health} to ${character2.health - attack}`);
+        },
+    };
+    character.randomizeHealth();
+    return character;
+} 
+
+/*
+7. Generate Multiple Characters
+    - made a loop to make a random character depending on how many they input and store those in an array.
+*/
+function generateMultipleCharacters(num){
+    let characters = []; 
+    for(let i = 0 ; i < num; i ++){
+        characters.push(generateCharacter());
     }
-};
+    return characters;
+}
 
-let warrior2 = {
-    name: "Zeus" ,
-    hp: 100,
-    strength: 20,
-    attack() {
-        return Math.floor(Math.random() * (this.strength + 1));
-    }
-};
-
-/* 2. Simulate 10 Rounds of Battle */
-/* Made the warrior 1 attack first then check if the hp is zero, if it is, the battle ends with the announcement of the winner.
-The same goes with warrior2, the loop ends when its already round 10 or when one of the warriors has an hp of zero.*/
-console.log("=== Battle Start! ===");
-
-for(let i = 1; i <= 10 ; i++){
-    console.log(`=== Round ${i} ===`);
-
-    /* Warrior1 Attacking */
-    let attackWarrior1 = warrior1.attack();
-    warrior2.hp = warrior2.hp - attackWarrior1;
-    console.log(`${warrior1.name} attacks ${warrior2.name} and does ${attackWarrior1} damage!`);
-    console.log(`${warrior1.name} HP: ${warrior1.hp} | ${warrior2.name} HP: ${warrior2.hp}`);
-
-    if (warrior2.hp <= 0){
-        console.log(`=== ${warrior1.name} WINS THE BATTLE!! === =`);
-        break;
-    };
-
-    /* Warrior2 Attacking */
-    let attackWarrior2 = warrior2.attack();
-    warrior1.hp = warrior1.hp - attackWarrior2;
-    console.log(`${warrior2.name} attacks ${warrior1.name} and does ${attackWarrior2} damage!`);
-    console.log(`${warrior1.name} HP: ${warrior1.hp} | ${warrior2.name} HP: ${warrior2.hp}`);
-    
-    if (warrior1.hp <= 0){
-        console.log(`=== ${warrior2.name} WINS THE BATTLE!! ===`);
-        break;
-    };
-
-    /* 3. Determine the Winner */
-    if(warrior1.hp > warrior2.hp){
-        console.log(`=== ${warrior1.name} WINS THE BATTLE! ===`);
-    }else if(warrior2.hp > warrior1.hp){
-        console.log(`=== ${warrior2.name} WINS THE BATTLE! ===`);
-    }else{
-        console.log("=== ITS A DRAW! ===");
-    };
-};
-
-
+/* Logs to make sure and check that it is working */
+const character1 = generateCharacter("Zane");
+const character2 = generateCharacter("Elara");
+console.log(character1, character2);
+character2.battle(character1);
+const party = generateMultipleCharacters(5);
+console.dir(party);
