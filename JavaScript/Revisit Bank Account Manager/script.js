467df60e3958e974b634c8865e818d9063ef78c9
czymanz9 <czyrahmanzano@gmail.com>
2025-03-09 10:27:14 +0800

    color selector

--- JavaScript/Revisit Bank Account Manager/script.js
+++ JavaScript/Revisit Bank Account Manager/script.js
@@ -4,6 +4,7 @@ Name: Czyrah Manzano
 
 /* Default Balance */
 let balance = 5000;
+let dailyLimit = 0;
 var currentBalance = document.getElementById("currentBalance");
 currentBalance.innerText ="Current Balance: $" + balance;
 
@@ -20,40 +21,84 @@ function error(){
     error.style.color = "red";
 }
 
+/* Display Feedback on transactions */
+function successMessage(userAction, userInput){
+    var depositMessage = document.getElementById("message");
+    depositMessage.innerHTML =`<h5><strong>Successfully ${userAction} $${userInput}</strong></h5>`;
+    depositMessage.style.color = "green";
+}
+
+/* Transaction Function shows the transaction the user made whether if its
+deposit or withdraw */
+function transaction(userAction, userInput){
+    var historyContainer = document.getElementById("transactionHistory")
+
+    var depositHistory = document.createElement("li");
+    depositHistory.className = "list-group-item list-group-item-secondary";
+    depositHistory.innerText = `${userAction}: $${userInput}`;
+
+    historyContainer.appendChild(depositHistory);
+}
+
 /* Deposit Function, gets the user input and made into a number so that when adding to the
 default balance it will sum it. Also made a message that tells that user has successfully
 deposited the input they entered. In addition an error function is included when user inputs an empty string
 or 0 or less so that they could only input a whole number. */
 function deposit(){
-    let userInput = parseFloat(document.getElementById("userInput").value)                                                        ;
+    let userInput = parseFloat(document.getElementById("userInput").value);
 
     if(isNaN(userInput)|| userInput <= 0 ){
         error();
-    }else{
-    balance += userInput
-
-    var depositMessage = document.getElementById("message");
-    depositMessage.innerHTML =`<h5><strong>Successfully Deposited $${userInput}</strong></h5>`;
-    depositMessage.style.color = "green";
-
-    currentBalance.innerText = "Current Balance: $" + balance;
+        return;
     }
+    
+    balance += userInput;
+
+    /* Success Message */
+    successMessage("deposited", userInput);
+
+    /* Transaction History */
+    transaction("Deposit", userInput);
+
+    /* Current Balance after deposit */
+    currentBalance.innerText = "Current Balance: $" + balance;
 }
 
-/* Function is same with deposit but minus the user input to the current balance */
+
+/* Withdraw Function has a daily limit of 500, in this function it checks it the withdrawal is within the balance
+and the rest is just the same as the deposit function with the display messages and transaction history.*/
 function withdraw(){
-    let userInput = parseFloat(document.getElementById("userInput").value)                                                        ;
+    let userInput = parseFloat(document.getElementById("userInput").value);
 
     if(isNaN(userInput)|| userInput <= 0 ){
         error();
-    }else{
-        
-    balance -= userInput
-
-    var depositMessage = document.getElementById("message");
-    depositMessage.innerHTML =`<h5><strong>Successfully Withdrew $${userInput}</strong></h5>`;
-    depositMessage.style.color = "green";
-
-    currentBalance.innerText = "Current Balance: $" + balance;
+        return;
     }
+    
+    /* Daily Limit Checker */
+    if (dailyLimit + userInput > 500) {
+        let limitMessage = document.getElementById("message");
+        limitMessage.innerHTML = `<h5><strong>Daily withdrawal limit reached!</strong></h5>`;
+        limitMessage.style.color = "red";
+        return; 
+    }
+
+    /* Balance Checker */
+    if (userInput > balance) { 
+        document.getElementById("message").innerHTML = `<h5><strong>Insufficient Balance!</strong></h5>`;
+        document.getElementById("message").style.color = "red";
+        return;
+    }
+    
+    balance -= userInput;
+    dailyLimit += userInput;
+
+    /* Success Message */
+    successMessage("withdrew", userInput);
+
+    /* Transaction History */
+    transaction("Withdraw", userInput);
+
+    /* Current Balance after deposit */
+    currentBalance.innerText ="Current Balance: $" + balance;
 }
